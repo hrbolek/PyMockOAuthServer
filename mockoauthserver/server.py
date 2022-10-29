e0779444df0922ba9cf41b004621ea9f9ef20d94
Profesor Hrbolek <profesor@hrbolek.cz>
2022-10-29 23:41:45 +0200

    Correction

--- mockoauthserver/server.py
+++ mockoauthserver/server.py
@@ -49,13 +49,13 @@ def loginPage(key):
         <form action="./login" method="post">
             <div class="mb-3">
             <label for="username" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="username" aria-describedby="emailHelp">
+            <input type="email" class="form-control" id="username" name="username" aria-describedby="emailHelp">
             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
             <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password">
-            <input type="hidden" class="form-control" id="key" value={key}>
+            <input type="password" class="form-control" id="password" name="password">
+            <input type="hidden" class="form-control" id="key" name="key" value={key}>
             </div>
             <button type="submit" class="btn btn-primary">Login</button>
         </form>
